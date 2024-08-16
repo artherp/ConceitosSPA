496cdc96160530829c85d3332aea2f806316d905
Arthur Erpen <77343288+artherp@users.noreply.github.com>
2024-08-16 20:56:39 -0300

    Adicionando um servidor local e simples com NodeJS

--- js/router.js
+++ js/router.js
@@ -1,30 +1,27 @@
-class Router {
-
-    routes = {}
-
+export class Router {
+    routes = {} 
+  
     add(routeName, page) {
-        this.routes[routeName] = page
+      this.routes[routeName] = page
     }
-
+    
     route(event) {
-        event = event || window.event
-        event.preventDefault()
-    
-        window.history.pushState({}, "", event.target.href)
-    
-        this.handle()
+      event = event || window.event
+      event.preventDefault()
+  
+      window.history.pushState({}, "", event.target.href)
+  
+      this.handle()
     }
-
-     handle() {
-        const { pathname } = window.location
-        const route = this.routes[pathname] || this.routes[404]
-    
-        console.log('antes do fetch')
-    
-        fetch(route).then(data => data.text())
-        .then(html => 
-        document.querySelector('#app').innerHTML = html)
+  
+    handle() {
+      const { pathname }  = window.location
+      const route = this.routes[pathname] || this.routes[404]
+      fetch(route)
+      .then(data => data.text())
+      .then(html => {
+        document.querySelector('#app').innerHTML = html
+      })
     }
-}
-
-export default new Router()
+  
+  }
